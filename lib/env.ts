e3d15ca50013e62efd70bc5d3f50db9ab3a3371e
cuonglm6 <cuonglm@pamoteam.com>
2026-04-29 15:33:00 +0700

    tích hơp mera

--- lib/env.ts
+++ lib/env.ts
@@ -3,3 +3,6 @@ export const GOOGLE_CLIENT_ID =
   process.env.GOOGLE_CLIENT_ID || "77812481890-js45lafkl9q57l9b8r14bo8u8qakar5v.apps.googleusercontent.com"
 export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET || "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"
 export const APP_JWT_SECRET = process.env.APP_JWT_SECRET || "9i3TV7kvul3gCa5eBM4V"
+
+export const MERA_API_URL = process.env.MERA_API_URL || "https://mera-fulfill-api.pamoteam.top"
+export const MERA_INTERNAL_API_KEY = process.env.MERA_INTERNAL_API_KEY || "97e36025fea1827de9236f23fc30ec3a2c5c4b560dd0c933aa9c48982afcca4e"
